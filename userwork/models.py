7a93c5a4b58a7d29cffc45de5b5ed9985847e62b
MaxOgarev <genombook@gmail.com>
2022-05-17 20:30:31 +0300

    Пробуем через Pycharm

---
+++ userwork/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Autor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField('Имя', max_length=64)
+    last_name = models.CharField('Фамилия', max_length=64)
+    email_autor = models.EmailField('Email', unique=True, max_length=64)
+
+    def __str__(self):
+        return f'Пользовательь {self.user.username}'
+
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+
